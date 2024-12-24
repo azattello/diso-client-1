4b32127a27aee9f2102e11755eaac4861fd4f920
Abdykali Azat <azat.abdykalli@gmail.com>
2024-12-24 18:32:16 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://diso-logistic.kz:3001',
+   
+    // nameCargo: 'TURKESTAN KARGO'
+    nameCargo: 'Din07express'
+  };
+  
+  
+export default config;
